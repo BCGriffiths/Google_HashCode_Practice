38de38e6af14ff763f385053d3426a78292fddb2
Brendan Griffiths <griffiths.bcfa@gmail.com>
2022-01-31 22:15:55 +0200

    Fix bug, not using schedule

--- intersection.py
+++ intersection.py
@@ -3,7 +3,7 @@ from asyncio.windows_events import NULL
 
 class intersection:
     id = 0
-    schedule_street_indices = []
+    schedule = []
     in_streets = []
     curr_street = 0
     curr_time = 0
@@ -12,8 +12,8 @@ class intersection:
         if self.current_time == self.curr_street.robot.time:
             self.curr_street += 1
             self.current_time = 0
-            self.in_streets[self.curr_street].robot.state = True
-            self.in_streets[self.curr_street - 1].robot.state = False
+            self.in_streets[self.schedule[self.curr_street]].robot.state = True
+            self.in_streets[self.schedule[self.curr_street - 1]].robot.state = False
         else :
             self.current_time += 1
-            self.in_streets[self.curr_street].robot.queue.top.moveAcrossIntersection()
+            self.in_streets[self.schedule[self.curr_street]].robot.queue.top.moveAcrossIntersection()
