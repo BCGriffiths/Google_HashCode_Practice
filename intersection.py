fdd370f3461298f298d5164eac54232757933499
Brendan Griffiths <griffiths.bcfa@gmail.com>
2022-01-31 22:32:10 +0200

    Add curr_street to loop back to 0

--- intersection.py
+++ intersection.py
@@ -12,8 +12,12 @@ class intersection:
         if self.current_time == self.curr_street.robot.time:
             self.curr_street += 1
             self.current_time = 0
-            self.in_streets[self.schedule[self.curr_street]].robot.state = True
-            self.in_streets[self.schedule[self.curr_street - 1]].robot.state = False
+            self.in_streets[self.schedule[self.curr_street]].robot.state = True #set next robot in shcedule to green
+            self.in_streets[self.schedule[self.curr_street - 1]].robot.state = False #Set previous robot in schedule to red
+
+            if(self.curr_street > len(self.schedule)) :
+                self.curr_street = 0
+
         else :
             self.current_time += 1
             self.in_streets[self.schedule[self.curr_street]].robot.queue.top.moveAcrossIntersection()
